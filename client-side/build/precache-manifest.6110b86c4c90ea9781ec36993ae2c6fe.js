1b442fe1320ec4f1e5faf9da3edafd40f014818f
Lauren <TechnologicalType@MacBook-Air-2.local>
2020-06-25 20:09:51 -0400

    new location

---
+++ client-side/build/precache-manifest.6110b86c4c90ea9781ec36993ae2c6fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bf40dd5f6fd628226bfd7bfd93eeac7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4355a7615360b2e30392",
+    "url": "/static/css/main.20854d68.chunk.css"
+  },
+  {
+    "revision": "e126b5361194dbf9b833",
+    "url": "/static/js/2.fb8fe0d5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.fb8fe0d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4355a7615360b2e30392",
+    "url": "/static/js/main.d643d86c.chunk.js"
+  },
+  {
+    "revision": "6af373bd941c439ec83d",
+    "url": "/static/js/runtime-main.d7af95be.js"
+  }
+]);
